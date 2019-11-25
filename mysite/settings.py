a12ebcff4bf43e5e6503e34614dede591b54b65a
Anirban Datta <teertha.d87@gmail.com>
2019-11-25 05:12:03 +0000

    Added Domain name.

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,7 +29,9 @@ SECRET_KEY = config['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['anirbandatta.pythonanywhere.com']
+ALLOWED_HOSTS = ['anirbandatta.pythonanywhere.com',
+                 'www.anirban.blog',
+    ]
 
 
 # Application definition
